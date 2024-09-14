e116bd27193c120c513e2bc80a52f642bd7f6b0e
Melvin Manzi <162992121+Melvin-Manzi@users.noreply.github.com>
2024-09-14 15:18:47 +0200

    Add files via upload

---
+++ landingPage.js
@@ -0,0 +1,16 @@
+import Top from "./top";
+import Navbar from "./navbar";
+import Land from "./land";
+import Service from "./services";
+
+export default function LandingPage() {
+  return (
+    <div>
+     <Top />
+     <Navbar />
+     <Land/>
+     <Service />
+    </div>
+  );
+}
+
